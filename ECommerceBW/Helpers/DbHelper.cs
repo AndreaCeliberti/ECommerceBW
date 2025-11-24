32cdf872153f27955f3b82dcd464b87e0a6235a8
AlessioVisconti <alessiovisconti00@gmail.com>
2025-11-24 14:17:50 +0100

    Product

--- ECommerceBW/Helpers/DbHelper.cs
+++ ECommerceBW/Helpers/DbHelper.cs
@@ -1,6 +1,13 @@
-﻿namespace ECommerceBW.Helper
+﻿using Microsoft.AspNetCore.Connections.Features;
+using Microsoft.Data.SqlClient;
+using ECommerceBW.Helpers.Enums;
+using ECommerceBW.Models;
+using System.Data;
+
+namespace ECommerceBW.Helper
 {
     public class DbHelper
     {
+
     }
 }
